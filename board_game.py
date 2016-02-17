8d565c975c854e8eaaf2350e94d5f420ed402d1e
Vishwas K Singh <vishwasks32@yahoo.in>
2016-02-18 00:26:34 +0530

    My first games

---
+++ board_game.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from random import randint
+
+board = [] #Define the board
+
+for x in range(5):
+    board.append(["O"] * 5) #create board with "O"
+
+def print_board(board):
+    for row in board:
+        print " ".join(row) #print board with " " and "O"
+
+print "Let's play Battleship!"
+print_board(board)
+# Hide the ship in the ocean
+def random_row(board):
+    return randint(1, len(board) - 1)
+
+def random_col(board):
+    return randint(1, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+#print ship_row
+#print ship_col
+turn = 0
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        print "Game Over"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row-1][guess_col-1] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row-1][guess_col-1] = "X"
+    # Print (turn + 1) here!
+    print "No. of turns :" + str(turn + 1)
+    print_board(board)
+if turn == 3:
+    print "Game Over"
